24333053bbe404ee47eb66423363c2efde6277e3
chiri <chirih439@gmail.com>
2024-06-19 11:25:24 +0300

    Added placeholder Images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -3,8 +3,10 @@ This is a service to manage the image url so as to crop the image
 The image urls in the responses accept a crop prameter that allows one to resize the images.
 */
 
+import placeHolderImage from '../assets/placeholder.png'
+
 const cropImageURL = (url: string) => {
-  if (!url) return '';
+  if (!url) return placeHolderImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   const slicedURL = url.slice(0, index) + "crop/600/400/" + url.slice(index);
